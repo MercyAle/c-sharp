f92b5e6d78c7643e4778b5ebf5c95f74c88c1708
Mercy Paredes <Alejandra-paad@outlook.es>
2020-10-26 22:32:21 -0600

    ejercicio notas escolares

---
+++ Ejercicios/notas-escolares/Alumnos.cs
@@ -0,0 +1,15 @@
+public class Alumno
+
+{
+
+    public int codigo { get; set; }
+
+    public string nombre { get; set; }
+
+    void ingresarNota(){
+
+        
+
+    }
+
+}
---
+++ Ejercicios/notas-escolares/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+
+namespace notas_escolares
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Alumno Alumno1 = new Alumno();
+
+            Alumno1.codigo = 1;
+
+            Alumno1.nombre = "Ana";
+
+            Alumno1.ingresarNota();
+
+
+
+            Alumno Alumno2 = new Alumno();
+
+            Alumno2.codigo = 2;
+
+            Alumno2.nombre = "Alejandra";
+
+            Alumno2.ingresarNota();
+
+
+
+            Console.WriteLine("Alumno: " + Alumno1.nombre);
+
+            Console.WriteLine("Alumno: " + Alumno2.nombre);
+
+        }
+
+    }
+
+}
