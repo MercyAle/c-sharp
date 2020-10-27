925db4e5f1e0d512b5be321cad58121d9b07e77b
Mercy Paredes <Alejandra-paad@outlook.es>
2020-10-26 23:13:02 -0600

    ejercicio propiedades

---
+++ Ejercicios/05-propiedades/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+
+
+namespace propiedades
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Alumno a = new Alumno();
+
+            a.Id = 1;
+
+            a.Primernombre = "Juan";
+
+            a.Segundonombre = "Perez";
+
+
+
+            Alumno b = new Alumno();
+
+            b.Id = 2;
+
+            b.Primernombre =  "Maria";
+
+            b.Segundonombre = "Martinez";
+
+
+
+            Alumno c = new Alumno(3);
+
+            c.Primernombre = "Pedro";
+
+
+
+            Alumno d = new Alumno("Alejandra", Paredes);
+
+
+
+
+
+            Console.WriteLine(a.Id);
+
+            Console.WriteLine(b.Id);
+
+            Console.WriteLine(c.Id);
+
+            Console.WriteLine(d.Primernombre + " " + d.Segundonombre);
+
+        }
+
+    }
+
+}
